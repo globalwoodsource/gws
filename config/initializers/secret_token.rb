9f2a2fe31301caff86585b80f0a75d95f9650e15
globalwoodsource <tech@globalwoodsource.com>
2013-10-16 13:46:13 -0500

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gws13::Application.config.secret_token = 'dfe9d8f1b894d6712b529c8116e7a7d70f689010167de06d91a14cc408bb5e8ffdb48a38349a7672e4e2a530e18053487988507afb47f3b434a7678242b70388'
